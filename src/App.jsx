807ccbe3009d8385dbfc42518e87bdb29e8f11b7
Najmaldin Ishag <najmaldinosman@gmail.com>
2025-06-15 02:39:31 +0300

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,5 @@
 const App = () => {
-  return (
-    <div className="underline text-neutral-400 p-10 border border-light-red-500">
-      Hello world
-    </div>
-  );
+  return <div className="underline">Hello world</div>;
 };
 
 export default App;
