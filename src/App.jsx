4367d48ff55eac125ce517ffbcbb1e3760cafd40
Najmaldin Ishag <najmaldinosman@gmail.com>
2025-06-17 16:00:25 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import About from "./Section/About";
 import Hero from "./Section/Hero";
+import Portfolio from "./Section/Portfolio ";
 
 const App = () => {
   return (
     <main>
       <Hero />;
       <About />
+      <Portfolio />
     </main>
   );
 };
