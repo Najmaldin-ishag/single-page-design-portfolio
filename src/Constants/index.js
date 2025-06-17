a1f7b3885b6af08cd2c22e29c612afc0ddb67db9
Najmaldin Ishag <najmaldinosman@gmail.com>
2025-06-17 16:00:33 +0300

    Create index.js

---
+++ src/Constants/index.js
@@ -0,0 +1,27 @@
+export const imgGallery = [
+  {
+    index: 0,
+    imgPath: "./assets/image-slide-1.jpg",
+    alt: "portfolio image",
+  },
+  {
+    index: 1,
+    imgPath: "./assets/image-slide-2.jpg",
+    alt: "portfolio image",
+  },
+  {
+    index: 2,
+    imgPath: "./assets/image-slide-3.jpg",
+    alt: "portfolio image",
+  },
+  {
+    index: 3,
+    imgPath: "./assets/image-slide-4.jpg",
+    alt: "portfolio image",
+  },
+  {
+    index: 4,
+    imgPath: "./assets/image-slide-5.jpg",
+    alt: "portfolio image",
+  },
+];
