9dba7b35bd0db792c8847f670793dbab6520043b
Najmaldin Ishag <najmaldinosman@gmail.com>
2025-06-20 11:41:05 +0300

    Update About.jsx

--- src/Section/About.jsx
+++ src/Section/About.jsx
@@ -3,23 +3,23 @@ import Button from "../Components/button";
 const About = () => {
   return (
     <section className="container p-10 max-w-[69.375rem] mt-[7.5rem]">
-      <article className="flex justify-between items-center  gap-[7.5rem] max-sm:flex-col">
+      <article className="flex justify-between items-center  gap-[7.5rem] max-sm:gap-4 max-sm:flex-col">
         <img
           src="./assets/image-amy.webp"
           className="max-w-[27.8125rem] max-sm:w-full"
           alt="amy picture"
         />
         <div>
-          <h2 className="text-[2.5rem] font-[700]  leading-[130%] mb-[2rem]">
+          <h2 className="text-[2.5rem] font-[700]  leading-[130%] mb-[2rem] max-sm:text-center">
             I’m Amy, and I’d love to <br /> work on your next project
           </h2>
-          <p className="text-[1.125rem] font-[500] leading-[150%] mb-[2rem]">
+          <p className="text-[1.125rem] font-[500] leading-[150%] mb-[2rem] max-sm:text-center">
             I love working with others to create beautiful design solutions.
             I’ve designed everything from brand illustrations to complete mobile
             apps. I’m also handy with a camera!
           </p>
           <Button
-            className="bg-light-red-500 flex cursor-pointer justify-center items-center px-[3.75rem] rounded-full py-[1rem] text-white"
+            className="bg-light-red-500 flex cursor-pointer justify-center max-sm:mx-auto items-center px-[3.75rem] rounded-full py-[1rem] text-white"
             title="Free Consultation"
           />
         </div>
